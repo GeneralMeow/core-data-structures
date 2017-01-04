7f101b67553e4e8ae830efaa9f56f9fb821a0242
John Hopkins <johnthopkins@gmail.com>
2017-01-04 10:56:06 -0800

    enqueue, dequeue, front, back, isEmpty, and length functions and tests

--- spec/queue.js
+++ spec/queue.js
@@ -11,11 +11,114 @@ describe('Queue', () => {
     expect(Queue).to.be.a('function')
   })
 
-  describe('push()', () => {
-    it('pushes an element to the top of the stack.', () => {
+  describe('enqueue()', () => {
+    it('pushes an element to the queue.', () => {
       const queue = new Queue()
 
       expect(() => queue.enqueue('foo'))
         .to.alter(() => queue.length(), { from: 0, to: 1 })
     })
+
+    it('pushes an element to the back of the queue.', () => {
+      const queue = new Queue()
+      queue.enqueue('foo')
+
+      expect(queue.back())
+        .to.equal('foo')
+    })
+  })
+
+  describe('dequeue()', ()=> {
+    it('returns and removes the front element in the queue.', () => {
+      const queue = new Queue()
+      queue.enqueue('foo')
+
+      expect(() => queue.dequeue())
+        .to.alter(() => queue.length(), {from: 1, to: 0 })
+    })
+
+    it('returns null if the queue is empty.', () => {
+      const queue = new Queue()
+
+      expect(queue.dequeue())
+        .to.be.null
+    })
+    it('removes returned element from queue', () => {
+      const queue = new Queue()
+
+      expect(queue.dequeue())
+        .to.be.null
+    })
+  })
+
+  describe('front()', ()=> {
+    it('returns the front element in the queue.', () => {
+      const queue = new Queue()
+      queue.enqueue('foo')
+      queue.enqueue('bar')
+
+      expect(queue.front())
+        .to.equal('foo')
+    })
+
+    it('returns null if the queue is empty.', () => {
+      const queue = new Queue()
+
+      expect(queue.front())
+        .to.be.null
+    })
+  })
+
+  describe('back()', ()=> {
+    it('returns the back element in the queue.', () => {
+      const queue = new Queue()
+      queue.enqueue('foo')
+      queue.enqueue('bar')
+
+      expect(queue.back())
+        .to.equal('bar')
+    })
+
+    it('returns null if the queue is empty.', () => {
+      const queue = new Queue()
+
+      expect(queue.back())
+        .to.be.null
+    })
+  })
+
+  describe('isEmpty()', ()=> {
+    it('returns true if the queue is empty or false if not', () => {
+      const queue = new Queue()
+      queue.enqueue('foo')
+      queue.enqueue('bar')
+
+      expect(queue.isEmpty())
+        .to.be.false
+    })
+    it('returns true if the queue is empty or false if not', () => {
+      const queue = new Queue()
+
+      expect(queue.isEmpty())
+        .to.be.true
+    })
+
+  })
+
+  describe('length()', ()=> {
+    it('returns the number of elements in the queue', () => {
+      const queue = new Queue()
+      queue.enqueue('foo')
+      queue.enqueue('bar')
+
+      expect(queue.length())
+        .to.equal(2)
+    })
+    it('returns the number of elements in the queue', () => {
+      const queue = new Queue()
+
+      expect(queue.length())
+        .to.equal(0)
+    })
+  })
 })
--- src/queue.js
+++ src/queue.js
@@ -2,6 +2,40 @@
 
 export default class Queue {
   constructor(){
-
+    this.queue = []
+    this.size = 0
   }
+
+enqueue(item){
+  this.queue.unshift(item)
+  this.size++
+}
+
+dequeue(){
+  if(this.size > 0){
+    const element = this.queue.pop()
+    this.size--
+    return element
+  } else {return null}
+}
+
+front(){
+  if(this.size > 0){
+    return this.queue[this.size - 1 ]
+  } else {return null}
+}
+
+back(){
+  if(this.size > 0){
+    return this.queue[0]
+  } else {return null}
+}
+
+isEmpty(){
+  return (this.size === 0)
+}
+
+length(){
+  return this.size
+}
 }
